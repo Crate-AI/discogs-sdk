72da33a6e44bd234ca616b16a7cd8730c5845f39
Ahmed felfel <felfelahmeed@gmail.com>
2024-06-08 16:56:55 -0400

    profile resources type

---
+++ core/.changeset/fair-cobras-roll.md
@@ -0,0 +1,5 @@
+---
+"@crate.ai/discogs-sdk": patch
+---
+
+added new profile resources type to user identity type
--- core/src/auth/types.ts
+++ core/src/auth/types.ts
@@ -22,8 +22,43 @@ export declare type UserIdentityParams = {
 export declare type UserIdentityResponse = {
     id: number;
     username: string;
-    resource_url: string;
+    resource_url: ProfileResources;
     consumerName: string;
 };
 
-
+export declare type ProfileResources = {
+    id: number;
+    resource_url: string;
+    uri: string;
+    username: string;
+    name: string;
+    home_page: string;
+    location: string;
+    profile: string;
+    registered: string;
+    rank: number;
+    num_pending: number;
+    num_for_sale: number;
+    num_lists: number;
+    releases_contributed: number;
+    releases_rated: number;
+    rating_avg: number;
+    inventory_url: string;
+    collection_folders_url: string;
+    collection_fields_url: string;
+    wantlist_url: string;
+    avatar_url: string;
+    curr_abbr: string;
+    activated: boolean;
+    marketplace_suspended: boolean;
+    banner_url: string;
+    buyer_rating: number;
+    buyer_rating_stars: number;
+    buyer_num_ratings: number;
+    seller_rating: number;
+    seller_rating_stars: number;
+    seller_num_ratings: number;
+    is_staff: boolean;
+    num_collection: number;
+    num_wantlist: number;
+};
